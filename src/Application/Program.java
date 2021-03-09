afcfc7b77dc00dd2045c6b80404d601bf1506132
brunodanielcg <bgonvalves.br@gmail.comgit config --global user.email bgonvalves.br@gmail.com>
2021-03-09 19:23:05 -0300

    Versão inicial sem tratamento de exceções

---
+++ src/Application/Program.java
@@ -0,0 +1,38 @@
+package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Model.entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double balance = sc.nextDouble();
+			sc.nextLine();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+				Account acc = new Account (number, holder, balance, withdrawLimit);
+				
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		acc.withdraw(amount);	
+		
+		System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+		
+		sc.close();
+		}
+		
+	}
